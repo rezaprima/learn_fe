9faefeab96900f88044123612520533f1c9d6d4a
Reza Primardiansyah <reza.primardiansyah@bukalapak.com>
2022-08-09 10:42:46 +0700

    implement login in vue

---
+++ src/components/Login.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="login">
+	<h1>Login</h1>
+	<div class="form_field">
+		<div class="field_name">Username</div>
+		<input type="text" name="username">
+	</div>
+	<div class="form_field">
+		<div class="field_name">Password</div>
+		<input type="password" name="password" id="thepassword">
+	</div>
+	<div class="form_field">
+		<input type="checkbox" onclick="togglePasswordVisible(this)" id="toggleCheck"> Show Password
+	</div>
+	<div class="submit_btn">
+		<input type="submit" title="Login">
+	</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login'
+}
+</script>
+
+<style>
+	form_field{
+		display: block;
+	}
+	.field_name{
+		font-size: 12pt;
+	}
+	input{
+		font-size: large;
+	}
+	.submit_btn {
+		padding-top: 5px;
+		width: 100%;
+		margin: auto;
+	}
+	input[type=submit]{
+		margin: auto;
+		width: 50%;
+		background-color: #6ebe40;
+		border-radius: 4px;
+	}
+
+	input[type=text]{
+		border-radius: 4px;
+		border-color: gray;
+	}
+	input[type=password]{
+		border-radius: 4px;
+		border-color: gray;
+	}
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,45 @@
+<template>
+  <header class="navbar">
+	<header>
+		<div id="title"><a href="product_list.html" id="title">Jualanku</a></div>
+		<div id="shopping_cart"><img src="../assets/shopping-cart-white.svg" alt=""></div>
+		<div id="login"><a href="login.html"><img src="../assets/log-in.svg" alt=""></a></div>
+	</header>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
+
+<style scoped>
+	header{
+		background-color: #cc1b4a;
+		display: flex;
+		margin: auto;
+		justify-content: flex-end;
+		padding-top: 5px;
+	} 
+	#title{
+		flex-grow: 10;
+		flex-basis: auto;
+		padding-left: 10px;
+		color: white;
+		margin: auto;
+		font-weight: bolder;
+		text-decoration: none;
+	}
+	#shopping_cart{
+		display: inline;
+		flex-grow: 0;
+		flex-shrink: 1;
+	}
+	#login{
+		display: inline;
+		flex-grow: 0;
+		flex-shrink: 1;
+	}
+</style>
+
