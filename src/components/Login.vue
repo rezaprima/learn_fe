680861b03ba1cf8250fd9f6f9ae22a0ded5003b1
Reza Primardiansyah <reza.primardiansyah@bukalapak.com>
2022-08-18 17:15:33 +0700

    cleanup

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -3,70 +3,81 @@
     <h1>Login</h1>
     <div class="form_field">
       <div class="field_name">Username</div>
-      <input type="text" v-model="username" name="username">
+      <input type="text" v-model="username" name="username" />
     </div>
     <div class="form_field">
       <div class="field_name">Password</div>
-      <input :type="passwordType" v-model="password" name="password" id="thepassword">
+      <input
+        :type="passwordType"
+        v-model="password"
+        name="password"
+        id="thepassword"
+      />
     </div>
     <div class="form_field">
-      <input type="checkbox" v-model="showPassword" id="toggleCheck"> Show Password
+      <input type="checkbox" v-model="showPassword" id="toggleCheck" /> Show
+      Password
     </div>
     <div class="submit_btn">
-      <input type="button" title="Login" :disabled="loginNotAllowed" value="Login">
+      <input
+        type="button"
+        title="Login"
+        :disabled="loginNotAllowed"
+        value="Login"
+      />
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Login',
+  name: "Login",
   data() {
     return {
-      username: '',
-      password: '',
+      username: "",
+      password: "",
       showPassword: false,
-    }
+    };
   },
   computed: {
-    passwordType: function() {
-      return this.showPassword ? 'text' : 'password'
+    passwordType: function () {
+      return this.showPassword ? "text" : "password";
     },
-    loginNotAllowed: function() {
-      return !(this.username !== '' && this.password !== '')
-    }
-  }
-}
+    loginNotAllowed: function () {
+      return !(this.username !== "" && this.password !== "");
+    },
+  },
+};
 </script>
 
 <style>
-  form_field{
-    display: block;
-  }
-  .field_name{
-    font-size: 12pt;
-  }
-  input{
-    font-size: large;
-  }
-  .submit_btn {
-    padding-top: 5px;
-    width: 100%;
-    margin: auto;
-  }
-  input[type=button]{
-    margin: auto;
-    width: 50%;
-    background-color: #6ebe40;
-    border-radius: 4px;
-  }
+form_field {
+  display: block;
+}
+.field_name {
+  font-size: 12pt;
+}
+input {
+  font-size: large;
+}
+.submit_btn {
+  padding-top: 5px;
+  width: 100%;
+  margin: auto;
+}
+input[type="button"] {
+  margin: auto;
+  width: 50%;
+  background-color: #6ebe40;
+  border-radius: 4px;
+}
 
-  input[type=text]{
-    border-radius: 4px;
-    border-color: gray;
-  }
-  input[type=password]{
-    border-radius: 4px;
-    border-color: gray;
-  }
+input[type="text"] {
+  border-radius: 4px;
+  border-color: gray;
+}
+input[type="password"] {
+  border-radius: 4px;
+  border-color: gray;
+}
 </style>
