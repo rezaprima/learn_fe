2b4c4a23de126ed15a5a12244f743a937601a515
Reza Primardiansyah <reza.primardiansyah@bukalapak.com>
2022-08-24 17:03:40 +0700

    add product-list in index

---
+++ pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="body">
+    <nav-bar/>
+    <product-list />
+  </div>
+</template>
+
+<script>
+import ProductList from '../src/components/ProductList.vue'
+import NavBar from '../src/components/NavBar.vue'
+export default {
+  components: { NavBar, ProductList },
+  name: 'App',
+  data: function () {
+    return {
+      page: 'product-list',
+      product: {}
+    }
+  }
+}
+</script>
+
+<style>
+  body{
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    color: #2c3e50;
+  }
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
